d3af59b5f1b31363971b0de7f06cc73f5e468322
Tukhtamurodov Sardorbek <sardor.mobile.dev@gmail.com>
2022-06-12 22:11:15 +0500

    🛡 Code optimized 🛡

--- lib/permission_provider.dart
+++ lib/permission_provider.dart
@@ -14,14 +14,23 @@ enum Permissions {
 }
 
 class PermissionProvider with ChangeNotifier {
-  Position? position;
-
-  // PermissionStatus _locationStatus = PermissionStatus.denied;
-
+  // * Fields
+  Position? _position;
+  bool _locationIsEnabled = false;
   Permissions _permissionStatus = Permissions.granted;
 
-  Permissions get permissionStatus => _permissionStatus;
+  // * Getters & Setters
+  Position? get position => _position;
 
+  bool get locationIsEnabled => _locationIsEnabled;
+  set locationIsEnabled(bool isEnabled) {
+    if (isEnabled != _locationIsEnabled) {
+      _locationIsEnabled = isEnabled;
+      notifyListeners();
+    }
+  }
+
+  Permissions get permissionStatus => _permissionStatus;
   set permissionStatus(Permissions value) {
     if (value != _permissionStatus) {
       _permissionStatus = value;
@@ -29,6 +38,7 @@ class PermissionProvider with ChangeNotifier {
     }
   }
 
+  // * Methods
   Future<bool> requestPermission() async {
     // Request for permission
     final status = await Permission.location.request();
@@ -44,30 +54,58 @@ class PermissionProvider with ChangeNotifier {
     return false;
   }
 
+  Future<void> checkLocationService() async{
+    // Check if the location services are enabled.
+    locationIsEnabled = await Geolocator.isLocationServiceEnabled();
+  }
+
+  Future<void> requestToEnableLocationService() async{
+    await checkLocationService();
+
+    // In case the location services are disabled => request to enable it
+    if (!locationIsEnabled) {
+      await Geolocator.openLocationSettings();
+      return;
+    }
+    return;
+  }
 
   /// Location
   /// Check if the location permission is granted,
   /// if it's not granted then request it.
   /// If it's granted then get the user's current location data
-  Future<Position> getGeoLocationPosition() async {
-    // #Getting the position coordinates
-    bool isEnabled;
-
+  Future getGeoLocationPosition() async {
     final hasPermission = await requestPermission();
-
-    // Check if the location services are enabled.
-    isEnabled = await Geolocator.isLocationServiceEnabled();
+    await checkLocationService();
 
     // In case the location services are disabled => request to enable it
-    if (!isEnabled && hasPermission) {
+    if (!locationIsEnabled && hasPermission) {
       await Geolocator.openLocationSettings();
-      return Future.error('Location services are disabled.');
+      return;
     }
-
-    // In case it is granted => access the position of the device.
-    return await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
+    else if(locationIsEnabled && hasPermission){
+      // In case it is granted => access the position of the device.
+      _position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      notifyListeners();
+      return;
+    }
   }
 
+
+  // #Double tap to exit
+  Future<bool> onWillPopFunction() async{
+    DateTime? lastPressed;
+    final now = DateTime.now();
+    const maxDuration = Duration(seconds: 2);
+    final isWarning =  lastPressed == null || now.difference(lastPressed) > maxDuration;
+
+    if(isWarning){
+      lastPressed = DateTime.now();
+      // doubleTap(context);
+      return false;
+    }
+    return true;
+  }
 }
